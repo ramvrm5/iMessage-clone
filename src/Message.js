f76019d791c639b47e39c314334501f9699ae54f
ram verma <42866242+ramvrm5@users.noreply.github.com>
2022-12-02 12:21:38 +0530

    chnages in message file

--- src/Message.js
+++ src/Message.js
@@ -8,6 +8,7 @@ const Message = forwardRef(({id,contents:{
     timestamp,displayName,email,message,photo,uid}},
     ref
     ) => {
+        
     const user = useSelector(selectUser)
     return (
         <div ref={ref} className={`message ${user.email === email && "message__sender"}`}>
