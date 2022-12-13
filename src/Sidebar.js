a6dc2fb0e154aa46ea9069e490714a8972c2688d
ram verma <42866242+ramvrm5@users.noreply.github.com>
2022-12-13 15:20:20 +0530

    changes in file changes

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -10,7 +10,6 @@ import db, { auth } from './firebase';
 
 function Sidebar() {
     const user = useSelector(selectUser);
-    
     const [chats, setChats] = useState([]);
 
     useEffect(() => {
