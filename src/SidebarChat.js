869bf91a0ad6b10bea805bec5526b8b936ba6574
ram verma <42866242+ramvrm5@users.noreply.github.com>
2022-12-20 16:20:20 +0530

    changes in file changes

--- src/SidebarChat.js
+++ src/SidebarChat.js
@@ -34,10 +34,13 @@ function SidebarChat({id,chatName}) {
             <Avatar src={chatInfo[0]?.photo}/>
 
             <div className="sidebarChat__info">
-                
+
                 <h3>{chatName}</h3>
+
                 <p>{chatInfo[0]?.message}</p>
+
                 <small>{timeago.format(new Date(chatInfo[0]?.timestamp?.toDate()))}</small>
+                
             </div>
         </div>
     )
